3b4b3fc73eb688c843e757322813ec38cdefc3f3
Josue <josue@Zipcoders-MacBook-Pro-10.local>
2024-07-14 10:23:52 -0400

    halfway done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,109 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+    public class SinglyLinkedList {
+    private Node head;
+    private Node tail;
+    private int listSize;
+
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.listSize = 0;
+    }
+
+    public class Node {
+        private String data;
+        private Node next;
+
+        public Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+//    get -- returns the element at the specified index
+//    copy -- returns a new linked list containing the same values
+//            (look up deep versus shallow copy)
+//    sort -- sorts the list using your algorithm of choice.
+//    You must perform the sorting yourself (no fair using someone else's library)
+
+
+    public void add (String data){
+        Node node = new Node(data);
+        if (listSize == 0){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        listSize++;
+    }
+
+    public void remove (int exampleIndex){
+        if (this.head.data.equals(this.get(exampleIndex))) {
+            this.head = this.head.next;
+            this.listSize--;
+        }
+        Node currentNode = this.head;
+        while (currentNode.next != null){
+            if (currentNode.next.data.equals(this.get(exampleIndex))) {
+                currentNode.next = currentNode.next.next;
+                this.listSize--;
+            }
+            currentNode = currentNode.next;
+        }
+
+    }
+
+    public int size(){
+        return this.listSize;
+    }
+
+    public boolean contains(String example) {
+        for (int i = 0; i < this.size(); i++) {
+            if (this.get(i).equals(example)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //    find -- returns the element's index if it is in the list, -1 otherwise
+    public int find(String example){
+         for (int i = 0; i < this.size(); i++){
+             if (this.get(i).equals(example)){
+                 return i;
+             }
+         }
+              return -1;
+    }
+
+    public String get(int index){
+        if (index == 0){
+            return this.head.data;
+        }
+
+        int currentIndex = 0;
+        Node currentNode = this.head;
+        while (currentNode.next != null){
+            currentNode = currentNode.next;
+            if (++currentIndex == index){
+                return currentNode.data;
+            }
+        }
+        return null;
+    }
+
+
 }
+
+
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,76 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest() {
+        SinglyLinkedList exampleList = new SinglyLinkedList();
+        exampleList.add("Josue");
+        exampleList.add("Joshua");
+        exampleList.add("George");
+        int actual = exampleList.size();
+        int expected = 3;
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void removeTest() {
+        SinglyLinkedList exampleList = new SinglyLinkedList();
+        exampleList.add("Josue");
+        exampleList.add("Joshua");
+        exampleList.add("George");
+        exampleList.remove(1);
+        String actual = exampleList.get(1);
+        String expected = "George";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void contains() {
+        SinglyLinkedList exampleList = new SinglyLinkedList();
+        exampleList.add("Josue");
+        exampleList.add("Joshua");
+        exampleList.add("George");
+        boolean actual = exampleList.contains("George");
+        boolean expected = true;
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void sizeTest() {
+        SinglyLinkedList exampleList = new SinglyLinkedList();
+        exampleList.add("Josue");
+        exampleList.add("Joshua");
+        int actual = exampleList.size();
+        int expected = 2;
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void getTest() {
+        SinglyLinkedList exampleList = new SinglyLinkedList();
+        exampleList.add("Josue");
+        exampleList.add("Joshua");
+        String actual = exampleList.get(1);
+        String expected = "Joshua";
+        Assert.assertEquals(actual,expected);
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void sort() {
+    }
 }
